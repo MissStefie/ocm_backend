334f14cf48667379a8db07393c5b1a58de7fc96c
MissStefie <mariah.stefanie.delga@gmail.com>
2024-11-29 17:50:51 -0300

    Ajustando variables de entorno para conexion con BD de Raillway

--- db.js
+++ db.js
@@ -3,10 +3,18 @@ import dotenv from "dotenv";
 //dotenv.config({ path: "./server/.env" });
 dotenv.config();
 
+// export const pool = createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+// });
+
 export const pool = createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
